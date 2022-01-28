01f9a0da7ead973d48731e14317ac4cdbfe31761
Jędrzej Stańczewski <91730807+jedrzej-stanczewski@users.noreply.github.com>
2022-01-28 02:09:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,124 +1,258 @@
 #include <iostream>
 #include <conio.h>
+#include <Windows.h>
+#include <time.h>
 
 bool gameOver;
 const int width = 20;
 const int height = 20;
-int x, y, fruitX, fruitY, score;
-enum direction {STOP = 0, LEFT, RIGHT, UP, DOWN};
+int headX, headY, foodX, foodY, score;
+int tailX[100], tailY[100];
+int tailLength;
+enum direction { STOP = 0, LEFT, RIGHT, UP, DOWN };
 direction dir;
+int gameSpeed;
+
 void setup()
 {
-  gameOver = false;
-  dir = STOP;
-  x = width / 2;
-  y = height / 2;
-  fruitX = rand() % width;
-  fruitY = rand() % height;
-  score = 0;
+    gameOver = false;
+    dir = STOP;
+    headX = width / 2;
+    headY = height / 2;
+    foodX = rand() % (width + 1);
+    foodY = rand() % height;
+    score = 0;
 }
 
 void draw()
 {
-  // cls for windows, clear for linux/macos
-  #ifdef _WIN32
-  system("cls);")
-  #else
-  system("clear"); 
-  #endif
+    // cls for windows, clear for linux/macos
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 
-  for (int i = 0; i < width; i++) 
-  {
-    std::cout << '#';
-  }
-
-  std::cout << std::endl;
-
-  for (int i = 0; i < height; i++)
-  {
-    for (int j = 0; j < width; j++)
+    // drawing top wall (first wall of #)
+    for (int i = 0; i < width + 1; i++)
     {
-      if (j == 0 || j == width - 1) 
-      {
         std::cout << '#';
-      } 
-      if (i == y && j == x) 
-      {
-        std::cout << "O";
-      } 
-      else if (i == fruitY && j == fruitX) 
-      {
-        std::cout << "F";
-      } 
-      else 
-      {
-        std::cout << " ";
-      }
     }
-    std::cout << std::endl;
-  }
 
-  for (int i = 0; i < width; i++) 
-  {
-    std::cout << '#';
-  }
-  std::cout << std::endl;
+    std::cout << "\n";
+
+    for (int i = 0; i < height; i++) // for each row
+    {
+        for (int j = 0; j < width; j++) // for each column in a row
+        {
+            if (j == 0 || j == width - 1) // first and last column
+            {
+                std::cout << '#';  // drawing left and right wall
+            }
+            if (i == headY && j == headX)
+            {
+                std::cout << "O";  // drawing snake head
+            }
+            else if (i == foodY && j == foodX)
+            {
+                std::cout << "o";  // drawing food
+            }
+            else
+            {
+                bool draw = false;
+                for (int k = 0; k < tailLength; k++) //  iterating over snake's tail
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        std::cout << "o";  // drawing snake tail
+                        draw = true;
+                    }
+                }
+                if (draw == false)
+                {
+                    std::cout << " ";  // basically rest of the map
+                }
+            }
+        }
+        std::cout << "\n";
+    }
+
+    for (int i = 0; i < width + 1; i++)  // for each column in the last row
+    {
+        std::cout << '#';  // drawing bottom wall
+    }
+    std::cout << "\n";
+    std::cout << "score: " << score << "\n";
+    std::cout << "To quit the game, press X" << "\n";
 }
 
-void input() 
+void input()
 {
-  if (_kbhit()) 
-  {
-    switch (_getch())
+    // keyboard presses defined
+    if (_kbhit())
     {
-      case 'a':
-        dir = LEFT;
-        break;
-      case 'd':
-        dir = RIGHT;
-        break;
-      case 'w':
-        dir = UP;
-        break;
-      case 's':
-        dir = DOWN;
-        break;
-      case 'x':
-        gameOver = true;
-        break;
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;  // 'x' stops the game
+            break;
+        }
     }
-  }
 }
 
 void logic()
 {
-  switch (dir)
-  {
+    // tracking the tail
+    int previousX = tailX[0];
+    int previousY = tailY[0];
+    int previous2X, previous2Y;
+    tailX[0] = headX; 
+    tailY[0] = headY;
+    for (int i = 1; i < tailLength; i++) // for each element of the tail
+    {
+        previous2X = tailX[i];
+        previous2Y = tailY[i];
+        tailX[i] = previousX;
+        tailY[i] = previousY;
+        previousX = previous2X;
+        previousY = previous2Y;
+    } 
+
+    // steering the snake
+    switch (dir)
+    {
     case LEFT:
-      x--;
-      break;
+        headX--;
+        break;
     case RIGHT:
-      x++;
-      break;
+        headX++;
+        break;
     case UP:
-      y--;
-      break;
+        headY--;
+        break;
     case DOWN:
-      y++;
-      break;
+        headY++;
+        break;
     default:
-      break;
-  }
+        break;
+    }
+
+    // wall collision
+    /*
+    if (headX > width - 2 || headX < 0)
+    {
+        gameOver = true;
+    }
+    if (headY > height || headY < 0)
+    {
+        gameOver = true;
+    } 
+    */
+
+    // no wall collision
+    if (headX >= width - 1)
+    {
+        headX = 0;
+    }
+    else if (headX < 0)
+    {
+        headX = width - 2;
+    }
+    if (headY >= height)
+    { 
+        headY = 0;
+    }
+    else if (headY < 0)
+    {
+        headY = height - 1;
+    }
+    for (int i = 0; i < tailLength; i++)
+    {
+        if (tailX[i] == headX && tailY[i] == headY)
+        {
+            gameOver = true;
+        }
+    }
+    // if snake eats the food
+    if (headX == foodX && headY == foodY)
+    {
+        score += 10;
+        foodX = rand() % width;
+        foodY = rand() % height;
+        tailLength++;
+    }
 }
 
-auto main() -> int 
+auto main() -> int
 {
-  setup();
-  while(!gameOver)
-  {
-    draw();
-    input();
-    logic();
-  }
-  return 0;
+    std::cout << "How difficult do You want the game to be? Type 1, 2, 3 or 4. 1 is the slowest mode, 4 is the fastest." << std::endl;
+    std::cin >> gameSpeed;
+    srand(time(NULL));
+    setup();
+    while (!gameOver)
+    {
+        if (gameSpeed == 1 || gameSpeed == 2 || gameSpeed == 3 || gameSpeed == 4) {
+            draw();
+            input();
+            logic();
+            if (gameSpeed == 1) {
+                if (dir == LEFT || dir == RIGHT)
+                {
+                    Sleep(100);
+                }
+                else if (dir == UP || dir == DOWN)
+                {
+                    Sleep(120);
+                }
+            }
+            else if (gameSpeed == 2) {
+                if (dir == LEFT || dir == RIGHT)
+                {
+                    Sleep(70);
+                }
+                else if (dir == UP || dir == DOWN)
+                {
+                    Sleep(90);
+                }
+            }
+            else if (gameSpeed == 3) {
+                if (dir == LEFT || dir == RIGHT)
+                {
+                    Sleep(30);
+                }
+                else if (dir == UP || dir == DOWN)
+                {
+                    Sleep(50);
+                }
+            }
+            else if (gameSpeed == 4) {
+                if (dir == LEFT || dir == RIGHT)
+                {
+                    Sleep(10);
+                }
+                else if (dir == UP || dir == DOWN)
+                {
+                    Sleep(30);
+                }
+            }
+        }
+        else
+        {
+            std::cout << "Improper value submitted or some other error occured." << "\n";
+            break;
+        }
+    }
+    return 0;
 }
